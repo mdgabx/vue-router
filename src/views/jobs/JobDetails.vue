090bbd18282f201fedd3c23021eb26e76bfd7886
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-02-03 21:31:46 +0800

    add: data fetching

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,11 +1,30 @@
 <template>
-    <h1>Job Details Page</h1>
-    <p>route id: {{ id }}</p>
+    <div v-if="job">
+        <h1>{{ job.title  }}</h1>
+        <p>route id: {{ id }}</p>
+        <p>{{ job.details }}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        props: ['id']
+        props: ['id'],
+        data() {
+            return {
+                job: null
+            }
+        },
+        mounted() {
+            this.fetchJob()
+        },
+        methods: {
+            fetchJob() {
+                fetch(`http://localhost:3000/jobs/${this.id}`)
+                    .then((res) => res.json())
+                    .then((data) => this.job = data)
+                    .catch((err) => console.error(err.message))
+            }
+        }
         // data() {
         //     return {
         //         id: this.$route.params.id
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,22 +1,35 @@
 <template>
   <h2>Jobs</h2>
-  <div v-for="job in jobs" :key="job.id">
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id">
     <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
       <h3> {{ job.title }}</h3>
     </router-link>
   </div>
+  </div>
+  <div v-else>
+      Loading...
+  </div>
+  
 </template>
 
 <script>
 export default {
     data() {
         return {
-            jobs: [
-                { title: 'Web developer', id: 1, details: 'Lorem' },
-                { title: 'CyberSecurity', id: 2, details: 'Lorem' },
-                { title: 'Data Scientist', id: 3, details: 'Lorem' },
-            ]
+            jobs: []
         }
+    },
+    mounted() {
+      this.fetchJobs()
+    },
+    methods: {
+      fetchJobs() {
+        fetch('http://localhost:3000/jobs')
+        .then((res) => res.json())
+        .then((data) => this.jobs = data)
+        .catch((err) => console.error(err.message))
+      }
     }
 }
 </script>
